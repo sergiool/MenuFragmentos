42c34579e6da267861eddfb742a40829cffd823c
Sérgio de Oliveira <sergiool@ufsj.edu.br>
2018-09-03 13:36:23 -0300

    Initial commit

---
+++ app/src/main/java/site/ufsj/menufragmentos/MainActivity.java
@@ -0,0 +1,70 @@
+package site.ufsj.menufragmentos;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.const_lay, new BlankFragment()).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id){
+            case R.id.action_fragment1:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.const_lay, new BlankFragment()).commit();
+                return true;
+            case R.id.action_fragment2:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.const_lay, new BlankFragment2()).commit();
+                return true;
+            case R.id.action_fragment3:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.const_lay, new BlankFragment3()).commit();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
